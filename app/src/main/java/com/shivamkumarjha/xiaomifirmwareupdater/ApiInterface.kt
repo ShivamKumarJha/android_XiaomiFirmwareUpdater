64663cdfe8271a8e4e906aef659bf04b245a2a32
ShivamKumarJha <jha.shivam3@gmail.com>
2020-09-10 01:54:16 +0530

    add interceptor
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/xiaomifirmwareupdater/ApiInterface.kt
+++ app/src/main/java/com/shivamkumarjha/xiaomifirmwareupdater/ApiInterface.kt
@@ -1,9 +1,12 @@
 package com.shivamkumarjha.xiaomifirmwareupdater
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
 
 interface ApiInterface {
     @GET("latest.yml")
@@ -14,7 +17,11 @@ interface ApiInterface {
             "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/data/"
 
         fun create(): ApiInterface {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
             val retrofit = Retrofit.Builder()
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
                 .build()
