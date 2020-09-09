e84d539118672d64ac36a532a780c44f036607a8
ShivamKumarJha <jha.shivam3@gmail.com>
2020-09-10 02:06:12 +0530

    ScalarService
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/xiaomifirmwareupdater/MainActivity.kt
+++ app/src/main/java/com/shivamkumarjha/xiaomifirmwareupdater/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,18 +18,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val apiInterface = ApiInterface.create().getPhones()
-        apiInterface.enqueue(object : Callback<Void> {
-            override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .baseUrl("https://your.base.url/")
+            .build()
+
+        val scalarService: ScalarService = retrofit.create(ScalarService::class.java)
+        val stringCall: Call<String> = scalarService.getStringResponse("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/data/latest.yml")
+        stringCall.enqueue(object : Callback<String?> {
+            override fun onResponse(call: Call<String?>?, response: Response<String?>) {
                 Log.d(TAG, "onResponse")
-                if (response?.body() != null) {
-                    Log.d(TAG, "response not null")
-                } else {
-                    Log.d(TAG, "response is null")
+                if (response.isSuccessful) {
+                    Log.d(TAG, "response.isSuccessful")
+                    if (response.body() != null) {
+                        Log.d(TAG, "response not null")
+                        Log.d(TAG, response.body().toString())
+                    } else {
+                        Log.d(TAG, "response is null")
+                    }
                 }
             }
 
-            override fun onFailure(call: Call<Void>?, t: Throwable?) {
+            override fun onFailure(call: Call<String?>?, t: Throwable?) {
                 Log.d(TAG, "onFailure")
             }
         })
